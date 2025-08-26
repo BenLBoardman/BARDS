5cdc21bd0b2ddabd879853143295ebc2f07aa745
BenLBoardman <benjaminboardman217@gmail.com>
2025-08-25 21:46:04 -0400

    Create multi bfs algo

---
+++ src/algo/MultiBFS.py
@@ -0,0 +1,11 @@
+import geopandas as gpd
+
+from src.obj.District import District
+
+import random
+from collections import deque
+
+class MultiBFS:
+
+    def draw(self, totPop: int, dists: list[District], numDists: int, gdf: gpd.GeoDataFrame):
+        pass
--- src/algo/Select.py
+++ src/algo/Select.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 
 from src.algo import SimpleBFS as sbfs
+from src.algo import MultiBFS as mbfs
 
 # Select an algorithm from the list of valid algorithms
 def selectAlgo(algo: str, population: int, numDists: int, gdf: gpd.GeoDataFrame):
@@ -8,6 +9,8 @@ def selectAlgo(algo: str, population: int, numDists: int, gdf: gpd.GeoDataFrame)
     # Insert new algorithms into the if-else HERE.
     if algo == "simple":
         drawer =  sbfs.SimpleBFS()
+    if algo == "multi":
+        drawer =  sbfs.MultiBFS()
     else:
         print(f"Algorithm {algo} not recognized, districts not drawn.")
         
