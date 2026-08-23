c96df41cc758016d9ebc6c5ba8669bc32c448450
Benjamin Boardman <67120619+BenLBoardman@users.noreply.github.com>
2026-08-23 15:34:32 -0400

    Rudimentary "report" in test algorithm

--- cppsrc/algo/mono_algo.cpp
+++ cppsrc/algo/mono_algo.cpp
@@ -10,7 +10,7 @@ void MonoDistrictTest::drawMap(State& s) {
 
     std::cout << "DISTRICTS COMPUTED. DISTRICT REPORT:" << std::endl;
     for(auto d : s.getDistricts()) {
-        std::cout << "DISTRICT " <<d->id << ": CONTIGUITY " << (d->isContiguous() ? "TRUE" : "FALSE") <<", TARGET POP " << d->target << ", ACTUAL POP " << d->getPopulation() <<std::endl;
+        std::cout << "DISTRICT " <<d->id << ": CONTIGUITY " << (d->isContiguous() ? "TRUE" : "FALSE") <<", TARGET POP " << d->targetPop << ", ACTUAL POP " << d->getPopulation() <<std::endl;
         std::cout << "\tCOMPACTNESS(P.P.)" << std::round(d->compactnessPolsbyPopper()*1000)/1000 
             << ", COMPACTNESS(R.)" << std::round(d->compactnessReock()*1000)/1000 << std::endl;
     }
